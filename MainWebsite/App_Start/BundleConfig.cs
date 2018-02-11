9ee279e25a77160d9c38f9cda93c7deb823cb806
kimikomu <kimikomu@gmail.com>
2018-02-11 15:29:30 -0500

    Add cdns to bundles and improve bundling

--- MainWebsite/App_Start/BundleConfig.cs
+++ MainWebsite/App_Start/BundleConfig.cs
@@ -11,49 +11,33 @@ namespace MainWebsite
             bundles.UseCdn = true;
             BundleTable.EnableOptimizations = true;
 #endif
-            // TODO: fix bundling to bring in from CDN
             bundles.Add(new StyleBundle("~/bundles/bootstrapcss", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css")
-                .Include("~/Content/bootstrap.min.css",
-                    "~/Content/font-awesome.min.css",
-                    "~/Content/Site.min.css"));
+                .Include("~/Content/bootstrap.min.css"));
 
             bundles.Add(new StyleBundle("~/bundles/fonts", "https://fonts.googleapis.com/css?family=Bree+Serif"));
 
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/Content/font-awesome.min.css",
+                "~/Content/Site.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr", "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js")
+                .Include("~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrapjs", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js")
-                .Include("~/Scripts/jquery-{version}.js",
-                    "~/Scripts/popper.min.js",
-                    "~/Scripts/jquery.validate*",
-                    "~/Scripts/jquery.validate.unobtrusive.min.js",
-                    "~/Scripts/modernizr-*",
-                    "~/Scripts/bootstrap.min.js",
-                    "~/Scripts/respond.min.js"));
+                .Include("~/Scripts/bootstrap.bundle.min.js",
+                "~/Scripts/respond.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.0/jquery.min.js")
+                .Include("~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval", "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js")
+                .Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/customscripts").Include(
-                        "~/Scripts/scripts"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryvalunob", "https://cdnjs.cloudflare.com/ajax/libs/jquery-validation-unobtrusive/3.2.6/jquery.validate.unobtrusive.min.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.min.js",
-            //          "~/Scripts/respond.min.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/customscripts").Include(
-            //          "~/Scripts/scripts"));
-
-            //bundles.Add(new StyleBundle("~/bundles/css").Include(
-            //          "~/Content/bootstrap.min.css",
-            //          "~/Content/font-awesome.min.css",
-            //          "~/Content/Site.min.css"));
+            bundles.Add(new ScriptBundle("~/bundles/customscripts")
+                .Include("~/Scripts/scripts"));
         }
     }
 }
