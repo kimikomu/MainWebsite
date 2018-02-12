ced8b8467af45ad352bb6e770d101ebb1fdf3d73
kimikomu <kimikomu@gmail.com>
2018-02-12 15:19:48 -0500

    Test Google Analytics in section scripts

---
+++ MainWebsite/Scripts/scripts.googleAnalytics.js
@@ -0,0 +1,20 @@
+﻿var gaHelper = (function ($) {
+    "use strict";
+    var module = {
+        notificationDetailsUrl: null,
+        onready: function () {
+            var keys = Object.keys(this);
+            var keysLength = keys.length;
+            for (var i = 0; i < keysLength; i++) {
+                if (this[keys[i]] === null) {
+                    throw new Error(keys[i] + " needs to be initialized.");
+                }
+            }
+        }
+
+        var onclick="ga('send', 'event', 'View Resume', 'click');"
+
+
+    };
+    return module;
+})(jQuery);
